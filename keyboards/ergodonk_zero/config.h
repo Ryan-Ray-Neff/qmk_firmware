99ab4ecba43adbeb54751b6de8662851a3e54303
Ryan Neff <ryan.ray.neff@fluke.com>
2023-12-14 11:33:06 -0800

    ergodonk_zero first commit

---
+++ keyboards/ergodonk_zero/config.h
@@ -0,0 +1,19 @@
+// Copyright 2023 Ryan Neff (@Ryan Neff)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define SERIAL_USART_FULL_DUPLEX
+#define SERIAL_USART_PIN_SWAP
+#define SERIAL_USART_TX_PIN GP0
+#define SERIAL_USART_RX_PIN GP1
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
+
+// Haptics 
+#define SOLENOID_PIN GP2
+#define SOLENOID_DEFAULT_DWELL 20
+#define SOLENOID_MIN_DWELL 4
+#define HAPTIC_OFF_IN_LOW_POWER 1
+#define NO_HAPTIC_MOD
---
+++ keyboards/ergodonk_zero/rules.mk
@@ -0,0 +1,3 @@
+SERIAL_DRIVER = vendor
+HAPTIC_ENABLE = yes
+HAPTIC_DRIVER = solenoid
