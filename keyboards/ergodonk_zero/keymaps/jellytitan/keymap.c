1caf1f1e1f33eaf55e00bdfa73fb8f7c823ddb93
JellyTitan <72168556+JellyTitan@users.noreply.github.com>
2025-01-03 14:29:25 -0800

    Adds string drop macro to jellytitan map

--- keyboards/ergodonk_zero/keymaps/jellytitan/keymap.c
+++ keyboards/ergodonk_zero/keymaps/jellytitan/keymap.c
@@ -35,6 +35,7 @@ enum custom_keycodes {
     CATSAFE,
     CMD_TAB_CW,
     CMD_TAB_CCW,
+    STR_DROP
 };
 
 /**
@@ -68,12 +69,12 @@ tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_split_6x9_6(
                 //┌────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┐
-                  TD_CATS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,   KC_F6,                       KC_F7,   KC_F8,   KC_F9  , KC_F10  , KC_F11  , KC_F12  , KC_DEL ,
-                  KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,   KC_5,    HF_TOGG, KC_VOLU,            KC_PSCR, KC_INS , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL ,KC_BSPC,
-                  KC_TAB ,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    HF_DWLU, KC_VOLD,            KC_SCRL, KC_DEL , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC,KC_BSLS,
+                  TD_CATS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,   KC_F6,                       KC_F7,   KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12  , KC_DEL ,
+                  KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,   KC_5,    HF_TOGG, KC_VOLU,            KC_PSCR, KC_INS , KC_6   , KC_7   , KC_8   , KC_9    , KC_0   , KC_MINS, KC_EQL ,KC_BSPC,
+                  KC_TAB ,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    HF_DWLU, KC_VOLD,            KC_SCRL, KC_DEL , KC_Y   , KC_U   , KC_I   , KC_O    , KC_P   , KC_LBRC, KC_RBRC,KC_BSLS,
                   LT(1,KC_CAPS),LT(2,KC_A),LCTL_T(KC_S),KC_D,LGUI_T(KC_F),KC_G,HF_DWLD,KC_MUTE,      KC_PAUS, KC_5   , KC_H,RGUI_T(KC_J),KC_K,RCTL_T(KC_L),LT(2,KC_SCLN),LT(1,KC_QUOT), KC_ENT,
-                  KC_LSFT,  LALT_T(KC_Z), KC_X,KC_C   , KC_V   , KC_B   , KC_MINI,KC_HOME,           KC_PGUP, KC_MAXI, KC_N   , KC_M   , KC_COMM, KC_DOT ,RALT_T(KC_SLSH), KC_RSFT,
-         KC_LCTL, KC_LGUI,  KC_LALT, KC_LEFT, KC_RGHT, KC_SPC, KC_BSPC, KC_END,                      KC_PGDN,  KC_TAB, KC_ENT , KC_UP  , KC_DOWN, KC_NO  , KC_RGUI, KC_RCTL,
+                  KC_LSFT,  LALT_T(KC_Z), KC_X,KC_C   , KC_V   , KC_B   , KC_MINI,KC_HOME,           KC_PGUP, KC_MAXI, KC_N   , KC_M   , KC_COMM, KC_DOT  , RALT_T(KC_SLSH), KC_RSFT,
+         KC_LCTL, KC_LGUI,  KC_LALT, KC_LEFT, KC_RGHT, KC_SPC, KC_BSPC, KC_END,                      KC_PGDN,  KC_TAB, KC_ENT , KC_UP  , KC_DOWN, STR_DROP, KC_RGUI, KC_RCTL,
                                                        QK_GESC,                   KC_ENT,             KC_6,                      KC_DEL
     ),
     [_LOWER] = LAYOUT_split_6x9_6(
@@ -158,6 +159,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 tap_code16(S(KC_TAB));
             } else {
                 unregister_code(KC_TAB);
+            }
+            break;
+         case STR_DROP:
+            if (record->event.pressed) {
+                 // when keycode QMKBEST is pressed
+                SEND_STRING("QMK is the best thing ever!");
             }
             break;
     }
